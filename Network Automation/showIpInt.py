3d38a8492db92fa25411b530bbaee55b1ed2308c
Ramzey Ghanaim <rgnet1@gmail.com>
2018-06-28 09:56:42 -0700

    added a function to read in a config file

--- Network Automation/showIpInt.py
+++ Network Automation/showIpInt.py
@@ -13,19 +13,11 @@ ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 #Read in file for address, username and password
-deviceList =[]
-numOfDevices = 0
-readfile = open("config.txt","r")
-for line in readfile:
-	line = line.strip("\r\n")
-	line = line.split(" ")
-	deviceList.append(line[0],line[1],line[2])
-	numOfDevices +=1
-
+numOfDevices, deviceList = readFile("config.txt")
 
 
 for i in range(numOfDevices):
-	print "********** Now Going into device: ",deviceList[i][1],"************"
+	print "********** Now Going into device: ",deviceList[i][1]," ************"
 	ssh.connect(deviceList[i][0],port=22, username=deviceList[i][1],
              	password=deviceList[i][2])
 
@@ -36,3 +28,15 @@ for i in range(numOfDevices):
 	#print the output
 	print '\n'.join(output)
 
+
+def readFile(fileName):
+	deviceList =[]
+	numOfDevices = 0
+	readfile = open("config.txt","r")
+	for line in readfile:
+		line = line.strip("\r\n")
+		line = line.split(" ")
+		deviceList.append(line[0],line[1],line[2])
+		numOfDevices +=1
+
+	return numOfDevices,deviceList
