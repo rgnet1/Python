5bbd7c91dd61dd268a7ddb9a28958680fd063704
Ramzey Ghanaim <rghanaim@intervision.com>
2018-09-10 18:09:40 -0700

    minor updates

--- Network Automation/showIpInt.py
+++ Network Automation/showIpInt.py
@@ -34,47 +34,51 @@ def execute_commands(ip_commands, ssh_remote, device_name):
 	
 	for line in command_list:
 		new_file = 'output-'+ device_name + '.txt'
-		result = open(new_file, 'w')
-		title = 'About to execute command: ' + line.strip()
-		top_bar = '           '
-		
-		for z in range(0,len(title), 1):
-			top_bar += '_'
-		print top_bar
+		result = open(new_file, 'w+')
+		cmd = line.strip()
+		if len(cmd) > 0:
+			title = 'About to execute command: ' + cmd
+			top_bar = '           '
 
-		curr_cmd = '__________|About to execute command: ' + line.strip() + '|__________'
-		print curr_cmd
-		
-		empty_line = '|'
-		for y in range(0, len(curr_cmd)-1,1):
-			empty_line +=' '
-		empty_line += '|'
-		print empty_line
-		#Now we can execute commands
-		ssh_remote.send(line.lstrip())
-		
-		time.sleep(3)
-		output = ssh_remote.recv(655350)
-		success_string = '| command status: successful'
-		invalid_string = '| command status: invalid'
-		if 'Invalid input' in output:
-			for u in range(0,len(curr_cmd) - len(invalid_string),1):
-				invalid_string += ' '
-			invalid_string += '|'
-			print invalid_string
-		else:
-			for u in range(0,len(curr_cmd) - len(success_string),1):
-				success_string += ' '
-			success_string += '|'
-			print success_string
-		
-		end_line = '|'
-		for x in range(0, len(curr_cmd)-1,1):
-			end_line += '_'
-		end_line += '|\n\n\n'
-		print end_line
+			for z in range(0, len(title), 1):
+				top_bar += '_'
+			print top_bar
 
-		result.write(output + '\n')
+			curr_cmd = '__________|About to execute command: ' + line.strip() + '|__________'
+			print curr_cmd
+
+			empty_line = '|'
+			for y in range(0, len(curr_cmd)-1,1):
+				empty_line +=' '
+			empty_line += '|'
+			print empty_line
+
+			#Now we can execute commands
+			ssh_remote.send(line.lstrip())
+
+			time.sleep(1)
+			output = ssh_remote.recv(655350)
+			success_string = '| command status: successful'
+			invalid_string = '| command status: invalid'
+			if 'Invalid input' in output or 'Incomplete command' in output:
+				for u in range(0,len(curr_cmd) - len(invalid_string), 1):
+					invalid_string += ' '
+				invalid_string += '|'
+				print invalid_string
+			else:
+				for u in range(0,len(curr_cmd) - len(success_string), 1):
+					success_string += ' '
+				success_string += '|'
+				print success_string
+
+			end_line = '|'
+			for x in range(0, len(curr_cmd)-1,1):
+				end_line += '_'
+			end_line += '|\n\n\n'
+			print end_line
+
+			result.write(output + '\n')
+			result.close()
 
 		
 
