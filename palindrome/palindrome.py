58d9099cc9c8d0080b3716959578b27ee91d5dcc
Ramzey <rgnet1@gmail.com>
2021-04-15 17:52:19 -0700

    Palindrome

---
+++ palindrome/palindrome.py
@@ -0,0 +1,16 @@
+# Palindrome is a string that can read forwards and backwards
+
+# Write a function that detects a Plindrome
+
+def isPalindrom(string):
+    left = 0
+    right = len(string) -1
+    while(right >= left):
+        if string[left] != string[right]:
+            return 0
+        left +=1
+        right -=1
+    return 1
+
+print(isPalindrom("racecar"))
+print(isPalindrom("cc."))
