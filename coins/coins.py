14a4daca94c709e3c9d6ef19f4823883a91e8fc4
Ramzey <rgnet1@gmail.com>
2021-04-16 11:31:10 -0700

    Added coins_availble data to allow flexability

--- coins/coins.py
+++ coins/coins.py
@@ -21,8 +21,8 @@ def coins(cents):
             result['coin_count'] +=1
     return result
 
-
-def coins_optimized(cents):
+# Gets number of coins based on cents and availble coins
+def coins_optimized(cents, coins_available):
     result = {
         25:0,
         10:0,
@@ -33,13 +33,32 @@ def coins_optimized(cents):
 
     coin_values = [25, 10, 5, 1]
     for value in coin_values:
+        if coins_available[value] == 0 :
+            continue
         coin_count = int(cents / value)
-        cents = cents % value
+        
+        exising_coins = coins_available[value]
+        if exising_coins < coin_count:
+            coin_count -= exising_coins
+            cents -= (coin_count * value)
+
         result[value] = coin_count
-        result['coin_count'] += coin_count
+        coins_available[value] -= coin_count
+        result['coin_count'] += coin_count 
+        cents = cents % value
+    
         if cents == 0:
             break
+            
+
     return result
 
-print(coins_optimized(138))
 
+coins_available = {
+        25:0,
+        10:100,
+        5:50,
+        1:35,
+}
+print(coins_optimized(138, coins_available))
+print("Remaining coins available:", coins_available)
